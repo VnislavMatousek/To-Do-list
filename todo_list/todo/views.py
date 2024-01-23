e1edfd86b1b3a7bb771f1f778c960173b6b93950
Vnislav Matoušek <vnislav.matousek@educanet.cz>
2024-01-23 07:41:24 +0000

    pridani

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+from .models import Note
+from .forms import NoteForm
 
 def index(request):
-    return render(request, "index.html")
+    notes = Note.objects.filter(user_id=request.user.id)
+    return render(request, "index.html", {"notes": notes})
+
+@login_required()
+def add_note(request):
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            messages.add_message(request, messages.INFO, "Věc byla přidána.")
+            return redirect("index")
+    else:
+        form = NoteForm()
+    return render(request, "add_note.html", {"form":form})
+
 
 def registration(request):
     if request.method == "POST":
@@ -15,3 +38,5 @@ def registration(request):
     else:
         form = UserCreationForm()
     return render(request, "registration/registration.html", {"form": form})
+
+
