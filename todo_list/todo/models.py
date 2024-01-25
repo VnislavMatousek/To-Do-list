4a1e8a2db2b82b20bb89175a458416bbeeda85c9
Vnislav Matoušek <vnislav.matousek@educanet.cz>
2024-01-25 17:10:15 +0000

    zaloha

--- todo_list/todo/models.py
+++ todo_list/todo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import markdown
+
 class Note(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
@@ -8,3 +10,6 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+
+    def formatted_description(self):
+        return markdown.markdown(self.description)
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
@@ -26,6 +26,30 @@ def add_note(request):
         form = NoteForm()
     return render(request, "add_note.html", {"form":form})
 
+@login_required()
+def edit_note(request, note_id):
+    note= get_object_or_404(Note,id=note_id)
+    if request.method == "POST":
+        form = NoteForm(request.POST, instance=note)
+
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.INFO, "Poznámka byla upravena.")
+            return redirect("index")  # Upravte název cesty podle vašich potřeb
+    else:
+        form = NoteForm(instance=note)
+
+    return render(request, "edit_note.html", {"form": form, "note": note})
+
+@login_required()
+def delete_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id, user=request.user)
+
+    note.delete()
+    messages.add_message(request, messages.INFO, "Poznámka byla smazána.")
+    return redirect("index")  # Upravte název cesty podle vašich potřeb
+
+    
 
 def registration(request):
     if request.method == "POST":
