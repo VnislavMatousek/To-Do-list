5e5960c7447bce5efc018e0bb77362ee1b0efe7e
Vnislav Matoušek <vnislav.matousek@educanet.cz>
2024-02-02 16:49:50 +0000

    testinko

--- todo_list/todo/admin.py
+++ todo_list/todo/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Note
+from .models import Note, Category
 
 # Register your models here.
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass
